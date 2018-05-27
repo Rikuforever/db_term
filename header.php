59a35712c1dbc4f8115596a99f2d75852e7d0be9
HoJoong Lee <rikuforever@korea.ac.kr>
2018-05-27 21:35:26 +0900

    fix navigation

--- header.php
+++ header.php
@@ -1,3 +1,9 @@
+<?
+function get_address(){
+    return substr($_SERVER['REQUEST_URI'],6);
+};
+?>
+
 <!DOCTYPE html>
 <html lang='ko'>
 <head>
@@ -37,15 +43,8 @@
     </div>
     <div class="w3-bar-block">
         <a href="#" class="w3-bar-item w3-button w3-padding-16 w3-hide-large w3-dark-grey w3-hover-black" onclick="w3_close()" title="close menu"><i class="fa fa-remove fa-fw"></i>  Close Menu</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding w3-blue"><i class="fa fa-users fa-fw"></i>  Overview</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-eye fa-fw"></i>  Views</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-users fa-fw"></i>  Traffic</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bullseye fa-fw"></i>  Geo</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-diamond fa-fw"></i>  Orders</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bell fa-fw"></i>  News</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-bank fa-fw"></i>  General</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-history fa-fw"></i>  History</a>
-        <a href="#" class="w3-bar-item w3-button w3-padding"><i class="fa fa-cog fa-fw"></i>  Settings</a><br><br>
+        <a href="player_list.php" class="w3-bar-item w3-button w3-padding <? if(get_address() == "player_list.php") echo "w3-blue"; ?>"><i class="fa fa-users fa-fw"></i>  Players</a>
+        <a href="hero_list.php" class="w3-bar-item w3-button w3-padding <? if(get_address() == "hero_list.php") echo "w3-blue"; ?>"><i class="fa fa-eye fa-fw"></i>  Heores</a>
     </div>
 </nav>
 
